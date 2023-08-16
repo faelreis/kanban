99cf5f7d228b27dd46fa0a0756c0dd85de11cb64
Rafael Reis Franco <rafaelreisfranco15@gmail.com>
2023-08-16 11:29:22 -0300

    Appwrite integration

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -5,7 +5,31 @@ export const getTodosGroupedByColumn = async () => {
         process.env.NEXT_PUBLIC_DATABASE_ID!,
         process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
     );
+
+
     const todos = data.documents
+    console.log(todos)
     
+    const columns = todos.reduce((acc, todo) => {
+        if(!acc.get(todo.status)){
+            acc.set(todo.status, {
+                id: todo.status,
+                todos: [],
+            })
+        }
+
+        acc.get(todo.status)!.todos.push({
+            $id: todo.$id,
+            $createAt: todo.createdAt,
+            title: todo.title,
+            description: todo.description,
+            tag: todo.tag,
+            status: todo.status,
+        })
+
+        return acc;
+
+    }, new Map<TypedColumn, Column>)
     
+    console.log(columns)
 }
